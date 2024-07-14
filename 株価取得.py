6e29f1727d8f53e008ceaa2d2b432f0ad63f32d8
hiro4747 <hirotaka4774@gmail.com>
2024-07-14 21:04:31 +0900

    コメント整理

--- 株価取得.py
+++ 株価取得.py
@@ -9,35 +9,12 @@ csv_file_path = 'C:\\Users\\kh111\\Desktop\\PythoTest\\銘柄.csv'
 
 # CSVファイルから1列目（A列）のデータを読み込んでリストに格納
 stock_symbols_jp = []
-#list = []のようにすると空の配列が作れる。
 
 with open(csv_file_path, newline='') as csvfile:
-#with open(ファイル名, 'w', encoding=文字エンコード） as 変数:
-#……(ファイルを操作する処理)
     
     reader = csv.reader(csvfile)
     for row in reader:
-        # A列のデータを取得し、リストに追加
-        #for row in rows: # for文で行を1つずつ取り出す(5)
-        #print(row[3]) # 商品名の列を指定のみ表示
         stock_symbols_jp.append(row[0])
-#リストのappend()メソッドで、末尾（最後）に要素を追加できる。先頭など、末尾以外の位置に追加したい場合は後述のinsert()を使う。
-#l = [0, 1, 2]
-#l.append(100)
-#print(l)
-# [0, 1, 2, 100]
-#l.append('abc')
-#print(l)
-# [0, 1, 2, 100, 'abc']
-
-#print(stock_symbols_jp)
-
-# 銘柄リスト（例：一部の企業の銘柄コード）
-#stock_symbols_jp = ["9984.T", "7203.T", "6758.T"]  # 任意の銘柄を指定        
-
-#株価コードを任意のCSVから反映できないかを検証中
-#1週間前からの株価データを取得（縦横入れ替え版）です
-
 
 # 一週間前の日付を計算
 end_date = datetime.today().strftime('%Y-%m-%d')
